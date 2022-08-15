8b383d4f1e4226796e8cfeafe0eed79f0c8e14c0
Hatgiargyrion <hat_ma360@hotmail.com>
2022-08-15 10:30:33 -0300

    Configuração do Runner, metodos Before e After.

---
+++ src/test/java/runners/CucumberRunner.java
@@ -0,0 +1,29 @@
+package runners;
+
+import core.WebDriverConfig;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features/",
+        glue = "steps",
+        plugin = {"pretty", "json:target/cucumber-reports/CucumberJson.json"},
+        monochrome = true,
+        tags = "@automobile_insurance")
+public class CucumberRunner {
+
+    private static final WebDriverConfig webDriverConfig = new WebDriverConfig();
+
+    @BeforeClass
+    public static void config() {
+        webDriverConfig.setUpBrowser("chrome").defaultConfig();
+    }
+
+    @AfterClass
+    public static void afterMethod(){
+        webDriverConfig.closeBrowser();
+    }
+}
